67d738ddda1d70d5e7e202beaa866b3606a7a29a
Christian Zaldaña <chris.zaldana92@gmail.com>
2024-10-07 23:56:10 -0600

    agregue sign out

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,11 @@ import '../src/assets/css/App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import AppRouter from './assets/router/AppRouter'
-import MainPage from './assets/components/MainPage'
-import { useState } from 'react'
-import { getAuth, onAuthStateChanged } from 'firebase/auth'
-import appFirebase from './assets/firebase/config'
-import Login from './assets/components/Login'
-const auth = getAuth(appFirebase)
+
 
 
 
 function App() {
-  const [usuario, setUsuario] = useState(null)
-
-onAuthStateChanged(auth,(usuarioFirebase) =>{
-  if (usuarioFirebase) {
-    setUsuario(usuarioFirebase)
-  }
-  else{
-    setUsuario(null)
-  }
-})
   return(
 
     <>
--- src/assets/components/MainPage.jsx
+++ src/assets/components/MainPage.jsx
@@ -1,13 +1,25 @@
 import { getAuth, signOut } from "firebase/auth"
 import appFirebase from "../firebase/config"
+import { Link, useNavigate } from "react-router-dom"
 const auth = getAuth(appFirebase)
 
 const MainPage = () => {
 
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    signOut(auth).then(() =>{
+      navigate('/')
+    }
+      
+    )
+  }
+
   return (
     <>
     <h1>Main Page</h1>
-    <button onClick={() => signOut(auth)}>Log out</button>
+    <Link to='/songplaying'> Esuchar una cancion </Link>
+    <button onClick={handleClick}>Log out</button>
     </>
   )
 }
