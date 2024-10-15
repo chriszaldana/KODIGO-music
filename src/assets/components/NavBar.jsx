cfa30a0c5a31cbfcc643c7daf3d96ae23b41871c
Christian Zaldaña <chris.zaldana92@gmail.com>
2024-10-15 12:06:09 -0600

    agregue boton menu

--- src/assets/components/NavBar.jsx
+++ src/assets/components/NavBar.jsx
@@ -3,6 +3,7 @@ import { useContext, useEffect, useState } from "react"
 import { Outlet, useLocation, useNavigate } from "react-router-dom"
 import { FavoriteContext } from "../context/FavoriteContext"
 import LogoIcon from '../images/LogoIcon.svg'
+import Menu from '../images/menu.svg'
 
 
 
@@ -56,7 +57,7 @@ const NavBar = () => {
             <div className="container-fluid">
                 <a className="navbar-brand text-light fs-3 fw-bolder" href={'/mainpage'}><img src={LogoIcon} alt="" /></a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
+                <span><img src={Menu} alt="" /></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
